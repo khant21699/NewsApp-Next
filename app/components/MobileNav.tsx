41d1ccf1d4bc4bdd7af4188255fed6723553db40
khant21699 <khant21699@hotmail.com>
2023-09-19 23:07:52 +0630

    responsive and bug fixes

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -14,7 +14,7 @@ const MobileNav = () => {
         className=" h-8 w-8 cursor-pointer hidden max-sm:block"
       />
       {showNav && (
-        <div className="fixed w-[50vw] h-[100vh] left-0 top-0 dark:bg-slate-900">
+        <div className="fixed w-[50vw] h-[200vh] left-0 top-0 dark:bg-slate-900">
           <div className="mt-4 items-center flex justify-around pb-4 border-b-[2px] border-orange-500 ">
             <h2 className="text-xl text-center ">Categories</h2>
             <XMarkIcon
